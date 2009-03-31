3831e44bd4826f034dd78824fecf752c766783fd
Andy Mikhailenko <andy@neithere.net>
2009-04-01 00:48:17 +0600

    Better support for setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  Copyright (c) 2008 Andy Mikhailenko and contributors
+#  Copyright (c) 2008-2009 Andy Mikhailenko and contributors
 #
 #  This file is part of Django Navigation.
 #
@@ -13,16 +13,20 @@
 " Django Navigation setup "
 
 from distutils.core import setup
-from navigation import get_version
+from navigation import __version__
 
 setup(
-    name         = 'Django Navigation',
-    version      = get_version(),
+    name         = 'django-navigation',
+    version      = __version__,
+    packages     = ['navigation'],
+    
+    install_requires = ['python >= 2.4', 'django >= 1.0'],
+    
     description  = 'A breadcrumbs navigation application for Django framework.',
     author       = 'Andy Mikhailenko',
     author_email = 'andy@neithere.net',
     url          = 'http://bitbucket.org/neithere/django-navigation/',
     download_url = 'http://bitbucket.org/neithere/django-navigation/src/',
     license      = 'GNU Lesser General Public License (LGPL), Version 3',
-    packages     = ['navigation'],
+    keywords     = 'django breadcrumbs navigation',
 )
