cca21b4d5e7f40900c67d96b8d2374e1295483a8
Andy Mikhailenko <andy@neithere.net>
2009-07-11 01:41:57 +0600

    Added trove classifiers for distribution

--- setup.py
+++ setup.py
@@ -19,9 +19,9 @@ setup(
     name         = 'django-navigation',
     version      = __version__,
     packages     = find_packages(exclude=['example']),
-    
+
     install_requires = ['django >= 1.0'],
-    
+
     description  = 'A breadcrumbs navigation application for Django framework.',
     author       = 'Andy Mikhailenko',
     author_email = 'andy@neithere.net',
@@ -29,4 +29,15 @@ setup(
     download_url = 'http://bitbucket.org/neithere/django-navigation/src/',
     license      = 'GNU Lesser General Public License (LGPL), Version 3',
     keywords     = 'django breadcrumbs navigation',
+    classifiers  = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Software Development :: Widget Sets',
+    ],
 )
