398b8913810172bee4102c67c4a2d4816a3b3672
Andy Mikhailenko <andy@neithere.net>
2009-11-03 03:08:57 +0500

    Added "breadcrumb" decorator (syntax sugar for the pre-existing approach).

---
+++ navigation/decorators.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright (c) 2008-2009 Andy Mikhailenko and contributors
+#
+#  Django Navigation is free software under terms of the GNU Lesser
+#  General Public License version 3 (LGPLv3) as published by the Free
+#  Software Foundation. See the file README for copying conditions.
+#
+
+def breadcrumb(crumb):
+    """
+    Usage:
+
+        from navigation.decorators import breadcrumb
+
+        @breadcrumb('greeting')
+        def some_view(request):
+            return 'Hello world!'
+
+        @breadcrumb(lambda request: 'greeting for %s' % request.user.username)
+        def some_view(request):
+            return 'Hello %s!' % request.user.username
+    """
+    def wrapper(view):
+        def inner(request, *args, **kwargs):
+            return view(request, *args, **kwargs)
+        inner.navigation = crumb              # TODO: rename "navigation" to "breadcrumb"
+        return inner
+    return wrapper
