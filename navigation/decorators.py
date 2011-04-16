166e544f6bb4e137359eaadb5ff1cc1534d2e432
Andy Mikhaylenko <andy@neithere.net>
2011-04-16 15:06:47 +0600

    Added support for coercion of (callable) breadcrumbs (to Unicode).

--- navigation/decorators.py
+++ navigation/decorators.py
@@ -15,7 +15,7 @@ from functools import wraps
 __all__ = ['breadcrumb']
 
 
-def breadcrumb(crumb):
+def breadcrumb(crumb, coerce_to=None):
     """
     Usage::
 
@@ -25,15 +25,35 @@ def breadcrumb(crumb):
         def some_view(request):
             return 'Hello world!'
 
-        @breadcrumb(lambda request: 'greeting for %s' % request.user.username)
+        @breadcrumb(lambda request: u'greeting for %s' % request.user.username)
         def some_view(request):
             return 'Hello %s!' % request.user.username
+
+    .. note::
+
+        By default the value returned by a callable is required to be a
+        ``unicode`` object. If the function returns a model instance, its
+        ``__unicode__`` method is *not* called. Use ``coerce_to=unicode``.
+
+    :param crumb:
+
+        A ``unicode`` string or a callable that returns it.
+
+    :param coerce_to:
+
+        Coerces *crumb* to given type. The value can be ``unicode`` or any
+        callable that returns a ``unicode`` object.
 
     """
     def wrapper(view):
         @wraps(view)
         def inner(request, *args, **kwargs):
             return view(request, *args, **kwargs)
-        inner.breadcrumb = crumb
+
+        if coerce_to is not None:
+            inner.breadcrumb = (
+                lambda *args, **kwargs: coerce_to(crumb(*args, **kwargs)))
+        else:
+            inner.breadcrumb = crumb
         return inner
     return wrapper
